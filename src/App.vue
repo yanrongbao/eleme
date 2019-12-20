feef6c523ae09219b45c4986ec5b783f0acc3f5f
yanrongbao <yanrongbao1566@163.com>
2019-12-20 15:58:24 +0800

    header header-detail 组件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <v-header :seller="seller"></v-header>
+        <header-detail :seller="seller"></header-detail>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VHeader from 'components/v-header/v-header.vue';
+import HeaderDetail from 'components/header-detail/header-detail.vue';
+import { getSeller } from 'api'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    data () {
+        return {
+            seller: {}
+        }
+    },
+    methods: {
+        _getSeller () {
+            getSeller().then(seller => {
+                this.seller = seller;
+            })
+        }
+    },
+    created () {
+        this._getSeller();
+    },
+    components: {
+        VHeader,
+        HeaderDetail
+    }
 }
 </script>
 
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+<style lang="stylus"></style>
